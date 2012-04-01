e779aa760e5439d472c8057a26d5746631d6c2cb
Nick Porillo <milkywayz@mail.com>
2012-04-01 13:45:03 -0400

    Fixed issue in plugin.yml and added a log message to console upon ranking

--- src/com/platymuus/bukkit/permissions/PermissionsRank.java
+++ src/com/platymuus/bukkit/permissions/PermissionsRank.java
@@ -1,14 +1,17 @@
 package com.platymuus.bukkit.permissions;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 class PermissionsRanker implements CommandExecutor {
 	private PermissionsPlugin plugin;
+	public static Logger log = Logger.getLogger("Minecraft");
 	public String noPermission = ChatColor.RED
 			+ "You don't have Permission to use this!";
 
@@ -21,6 +24,8 @@ class PermissionsRanker implements CommandExecutor {
 		try {
 			if (sender.hasPermission("permissions.rank." + split[1])) {
 				String player = split[0].toLowerCase();
+				Player target = plugin.getServer().getPlayer(split[0]);
+				String PlayerName = sender.getName();
 				String carget = plugin.getServer().getPlayer(split[0]).getDisplayName();
 				String[] groups = split[1].split(",");	
 				if (plugin.getNode("users/" + player) == null) {
@@ -30,7 +35,11 @@ class PermissionsRanker implements CommandExecutor {
 						Arrays.asList(groups));
 				plugin.refreshPermissions();
 				sender.sendMessage(ChatColor.GREEN + "You've changed " + carget
-						+ "'s rank to " + split[1] + ".");
+						+ "'s rank to " + split[1] + ".");			
+				target.sendMessage(ChatColor.GREEN + "[PB]" +ChatColor.AQUA + sender.getServer().getPlayer(PlayerName).getDisplayName() + " changed your rank to " +
+					  split[1] + "!");
+				log.info("[Permissions] " + sender.getServer().getPlayer(PlayerName).getDisplayName() 
+						+ " changed" + carget + "'s to " + split[1] + ".");
 				return true;
 			} else {
 				sender.sendMessage(this.noPermission);
